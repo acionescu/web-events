ed2a0c858bf08e83a37582924305636881e084ab
Adrian Ionescu <adrian.ionescu@snowglobegames.com>
2020-10-28 16:31:27 +0200

    add license header

--- src/main/java/net/segoia/eventbus/web/ws/v0/ServerWsEndpointV0.java
+++ src/main/java/net/segoia/eventbus/web/ws/v0/ServerWsEndpointV0.java
@@ -1,3 +1,19 @@
+/**
+ * web-events - An event-bus extension for events transmission over websockets
+ * Copyright (C) 2016  Adrian Cristian Ionescu - https://github.com/acionescu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.segoia.eventbus.web.ws.v0;
 
 import java.io.UnsupportedEncodingException;
@@ -77,7 +93,7 @@ public abstract class ServerWsEndpointV0 extends WsServerEndpointTransceiver {
     @Override
     public void receiveData(PeerDataEvent dataEvent) {
 	/* make sure we're not exceeding allowed activity level */
-	stats.onEvent(new EventContext(dataEvent, null));
+	stats.onEvent(new EventContext(dataEvent));
 	float activityIndex = stats.getActivityIndex();
 	logger.debug("Activity index: " + activityIndex);
 	if (activityIndex > maxAllowedActivity) {
